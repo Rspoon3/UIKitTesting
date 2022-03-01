361561196926ca19821e4539fc4b74a05c8bf7f4
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2022-03-01 12:18:22 -0500

    Initial Commit

---
+++ UIKitTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTesting
+//
+//  Created by Richard Witherspoon on 3/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
