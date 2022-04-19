cac9b1c3c19c3dfbba13a436465354e34212db13
Ricky <42879920+Rspoon3@users.noreply.github.com>
2022-04-19 11:23:02 -0400

    initial commit

--- UIKitTesting/ViewController.swift
+++ UIKitTesting/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import FontAwesome
+
 
 class ViewController: UIViewController {
+    let imageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage.fontAwesomeIcon(name: .github,
+                                                  style: .regular,
+                                                  textColor: .systemPurple,
+                                                  size: .init(width: 300, height: 300))
+        
+        
+        view.addSubview(imageView)
+                
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 400),
+            imageView.heightAnchor.constraint(equalToConstant: 400),
+        ])
     }
-
-
 }
 
