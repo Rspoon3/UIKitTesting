40202ec6eb6f2a3433d3d58f200d20183651342b
Ricky <42879920+Rspoon3@users.noreply.github.com>
2022-04-18 21:05:53 -0400

    initial commit

--- UIKitTesting/ViewController.swift
+++ UIKitTesting/ViewController.swift
@@ -6,14 +6,123 @@
 //
 
 import UIKit
+import LoremSwiftum
+//https://stackoverflow.com/questions/71918534/make-nscollectionlayoutitem-heights-as-tall-as-the-nscollectionlayoutgroup-when
 
 class ViewController: UIViewController {
-
+    enum Section {
+        case main
+    }
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    var collectionView: UICollectionView! = nil
+    let sectionBackgroundDecorationElementKind = "section-background-element-kind"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Two-Column Grid"
+        configureHierarchy()
+        configureDataSource()
     }
+    
+    /// - Tag: TwoColumn
+    func createLayout() -> UICollectionViewLayout {
+        let spacing = CGFloat(10)
 
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .estimated(44))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .estimated(44))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+        group.interItemSpacing = .fixed(spacing)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = spacing
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
 
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+    func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+    }
+    
+    func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<TextCell, Int> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = Lorem.sentences(Bool.random() ? 1 : 3)
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            // Return the cell.
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+        }
+        
+        // initial data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(Array(0..<94))
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
 }
 
+
+
+
+class TextCell: UICollectionViewCell {
+    let label = UILabel()
+    let bottomLabel = UILabel()
+    let container = UIView()
+    static let reuseIdentifier = "text-cell-reuse-identifier"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("not implemented")
+    }
+    
+
+    func configure() {
+        label.numberOfLines = 0
+        label.adjustsFontForContentSizeCategory = true
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        label.backgroundColor = .systemPurple
+        label.textColor = .white
+
+        
+        bottomLabel.numberOfLines = 0
+        bottomLabel.adjustsFontForContentSizeCategory = true
+        bottomLabel.font = UIFont.preferredFont(forTextStyle: .title1)
+        bottomLabel.text = "Bottom of cell"
+        bottomLabel.backgroundColor = .systemRed
+        bottomLabel.textColor = .white
+        
+        
+        let stack = UIStackView(arrangedSubviews: [label, bottomLabel])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.distribution = .equalSpacing
+        stack.spacing = 20
+        contentView.addSubview(stack)
+
+        backgroundColor = .systemBlue.withAlphaComponent(0.75)
+
+        let inset = CGFloat(10)
+        NSLayoutConstraint.activate([
+            stack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            stack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            stack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            stack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset),
+        ])
+    }
+}
