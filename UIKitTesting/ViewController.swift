191897b0d871c55e2e2a310ad81eca7abab35174
Ricky <42879920+Rspoon3@users.noreply.github.com>
2022-04-22 10:22:40 -0400

    how shows animated faceId view

--- UIKitTesting/ViewController.swift
+++ UIKitTesting/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
-
+//https://jordan-dixon.com/2018/02/03/using-apples-private-apis-with-swift/
 class ViewController: UIViewController {
-
+    var faceIDView: UIView!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        faceIDView.setValue(1, forKey: "state")
+        faceIDView.perform(NSSelectorFromString("_applyStateAnimated:"))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Bundle(path: "/System/Library/PrivateFrameworks/LocalAuthenticationPrivateUI.framework")?.load()
+        
+        let GlyphView = NSClassFromString("LAUIPearlGlyphView") as! UIView.Type
+        
+        faceIDView = GlyphView.init()
+        faceIDView.frame = .init(x: view.center.x - 50, y: view.center.y - 50, width: 100, height: 100)
+        view.addSubview(faceIDView)
     }
-
-
+    
+    
 }
 
