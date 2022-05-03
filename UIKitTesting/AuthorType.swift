8da6516546c00773c4c6c3e8de2160a895d76274
Ricky <42879920+Rspoon3@users.noreply.github.com>
2022-05-03 12:09:19 -0400

    initial commit

---
+++ UIKitTesting/AuthorType.swift
@@ -0,0 +1,13 @@
+//
+//  AuthorType.swift
+//  UIKitTesting
+//
+//  Created by Richard Witherspoon on 5/3/22.
+//
+
+import Foundation
+
+
+enum AuthorType {
+    case single, multiple
+}
--- UIKitTesting/ViewController.swift
+++ UIKitTesting/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var scrollView: UIScrollView!
+    private var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Books - Stack"
+        view.backgroundColor = UIColor.white
+        configureScrollView()
+        configureStackView()
+        loadData()
     }
-
-
+    
+    private func configureScrollView(){
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(scrollView)
+    }
+    
+    private func configureStackView(){
+        stackView = UIStackView(frame: scrollView.bounds)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.distribution = .equalSpacing
+        scrollView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leftAnchor.constraint(equalTo: scrollView.leftAnchor, constant: 16),
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 16),
+            stackView.rightAnchor.constraint(equalTo: scrollView.rightAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16),
+        ])
+    }
+    
+    private func loadData() {
+        for (index, section) in Book.sections.enumerated() {
+            var config = UIListContentConfiguration.plainHeader()
+            config.text = section
+            
+            var cv = UIListContentView(configuration: config)
+            stackView.addArrangedSubview(cv)
+            
+            for book in Book.booksFor(section: index) {
+                config = UIListContentConfiguration.cell()
+                config.image = book.authType == .single ? UIImage(systemName: "person.fill") : UIImage(systemName: "person.2.fill")
+                config.text = book.title
+                config.secondaryText = book.author
+                
+                cv = UIListContentView(configuration: config)
+                stackView.addArrangedSubview(cv)
+            }
+        }
+    }
 }
-
