12ebd283bdba78849577b67f9a6a3a3baf8c2f99
Ricky <42879920+Rspoon3@users.noreply.github.com>
2022-05-20 15:55:57 -0400

    initial commit

---
+++ UIKitTesting/Node.swift
@@ -0,0 +1,70 @@
+//
+//  Node.swift
+//  UIKitTesting
+//
+//  Created by Richard Witherspoon on 5/19/22.
+//
+
+import Foundation
+
+@resultBuilder
+struct NodeBuilder {
+    static func buildBlock<Value>(_ children: Node<Value>...) -> [Node<Value>] {
+        children
+    }
+}
+
+struct Node<Value>: Identifiable {
+    var value: Value
+    private(set) var children: [Node]
+    var id = UUID()
+    
+    mutating func add(child: Node) {
+        children.append(child)
+    }
+    
+    init(_ value: Value) {
+        self.value = value
+        children = []
+    }
+
+    init(_ value: Value, children: [Node]) {
+        self.value = value
+        self.children = children
+    }
+    
+    init(_ value: Value, @NodeBuilder builder: () -> [Node]) {
+        self.value = value
+        self.children = builder()
+    }
+    
+    var count: Int {
+        1 + children.reduce(0) { $0 + $1.count }
+    }
+    
+    var recursiveChildren: [Node] {
+        return children + children.flatMap { $0.recursiveChildren }
+    }
+    
+    func depthFirstTraversal(visit: (Node) -> Void) {
+        visit(self)
+        children.forEach {
+            $0.depthFirstTraversal(visit: visit)
+        }
+    }
+    
+}
+
+extension Node where Value == String {
+    func getHierarchy() -> [(Node<String>, Node<String>)] {
+        children.map{ node in
+            (self, node)
+        } + children.flatMap{ $0.getHierarchy() }
+    }
+}
+
+
+
+extension Node: Equatable where Value: Equatable { }
+extension Node: Hashable where Value: Hashable { }
+extension Node: Codable where Value: Codable { }
--- UIKitTesting/ViewController.swift
+++ UIKitTesting/ViewController.swift
@@ -8,12 +8,142 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var collectionView: UICollectionView! = nil
+    var dataSource: UICollectionViewDiffableDataSource<String, Node<String>>! = nil
+    
+    let root = Node("Terry") {
+        Node("Paul") {
+            Node("Sophie")
+            Node("Timmy")
+            Node("Sandra") {
+                Node("Aimee")
+                Node("Niki")
+            }
+            Node("Bob")
+        }
 
+        Node("Andrew") {
+            Node("John")
+            Node("Adam")
+            Node("Suzzie")
+            Node("Ricky"){
+                Node("Taylor")
+                Node("Megan")
+                Node("Arthur") {
+                    Node("Fred")
+                    Node("George")
+                    Node("Giny") {
+                        Node("Harry")
+                        Node("Harold")
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    //MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureCollectionView()
+        configureDataSource()
+        applySnapshot(animated: false)
+    }
+    
+    private func configureCollectionView() {
+        var config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        config.headerMode = .firstItemInSection
+        let layout = UICollectionViewCompositionalLayout.list(using: config)
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemBackground
+        
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
+    
+    
+    //MARK: - Data Source
+    func configureDataSource() {
+        let mainRegistration = createMainRegistration()
+        let headerRegistration = createHeaderRegistration()
+        
+        dataSource = UICollectionViewDiffableDataSource<String, Node<String>>(collectionView: collectionView) { (collectionView, indexPath, node) -> UICollectionViewCell? in
+            
+            return collectionView.dequeueConfiguredReusableCell(using: mainRegistration, for: indexPath, item: node)
+        }
+        
+        dataSource?.supplementaryViewProvider = { (view, kind, index) in
+            return self.collectionView.dequeueConfiguredReusableSupplementary(using:headerRegistration, for: index)
+        }
+    }
+    
+    private func createHeaderRegistration() -> UICollectionView.SupplementaryRegistration<UICollectionViewListCell> {
+        UICollectionView.SupplementaryRegistration <UICollectionViewListCell>(elementKind: UICollectionView.elementKindSectionHeader) { [weak self] (headerView, elementKind, indexPath) in
+            guard
+                let self = self,
+                let sectionTitle = self.dataSource?.snapshot().sectionIdentifiers[indexPath.section]
+            else { fatalError() }
+            
+            var configuration: UIListContentConfiguration!
+            
+            if #available(iOS 15.0, *) {
+                configuration = UIListContentConfiguration.prominentInsetGroupedHeader()
+            } else {
+                configuration = UIListContentConfiguration.sidebarHeader()
+            }
+            
+            configuration.text = sectionTitle
+            headerView.contentConfiguration = configuration
+        }
     }
 
-
+    
+    private func createMainRegistration()-> UICollectionView.CellRegistration<UICollectionViewListCell, Node<String>> {
+        UICollectionView.CellRegistration { (cell, indexPath, node) in
+            var content = cell.defaultContentConfiguration()
+            content.text = node.value
+            content.textProperties.numberOfLines = 0
+            
+            cell.contentConfiguration = content
+            
+            
+            cell.accessories = [.outlineDisclosure(displayed: .always,
+                                                   options: .init(style: .header, isHidden: node.children.isEmpty))]
+        }
+    }
+    
+    fileprivate func extractedFunc(_ child: Node<String>, _ sectionSnapshot: inout NSDiffableDataSourceSectionSnapshot<Node<String>>) {
+        for subChild in child.children {
+            sectionSnapshot.append(subChild.children, to: subChild)
+            extractedFunc(subChild, &sectionSnapshot)
+        }
+    }
+    
+    func applySnapshot(animated: Bool) {
+        
+        for child in root.children{
+            var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<Node<String>>()
+            sectionSnapshot.append([child])
+//            
+            sectionSnapshot.append(child.children, to: child)
+            
+            
+            extractedFunc(child, &sectionSnapshot)
+            
+            
+            sectionSnapshot.expand(sectionSnapshot.items)
+            dataSource.apply(sectionSnapshot, to: child.value, animatingDifferences: animated)
+        }
+    }
 }
+
+
 
